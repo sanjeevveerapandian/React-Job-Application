d56f96051a95a7d89f7c47f60ef4d17566239f92
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2024-07-01 00:59:32 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 const Navbar = () => {
   return (
@@ -6,35 +7,32 @@ const Navbar = () => {
         <div class="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             {/* <!-- Logo --> */}
-            <a
-              className="flex flex-shrink-0 items-center mr-4"
-              href="/index.html"
-            >
+            <Link className="flex flex-shrink-0 items-center mr-4" to="/">
               <img className="h-10 w-auto" src={logo} alt="React Jobs" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 React Jobs
               </span>
-            </a>
+            </Link>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <a
-                  href="/"
+                <Link
+                  to="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Home
-                </a>
-                <a
-                  href="/jobs"
+                </Link>
+                <Link
+                  to="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Jobs
-                </a>
-                <a
-                  href="/add-job"
+                </Link>
+                <Link
+                  to="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Add Job
-                </a>
+                </Link>
               </div>
             </div>
           </div>
